5b2510c62d4cb094a05efda6ba217c60b82c4971
abdelmonemanwr <abdelmonemanwr7777@gmail.com>
2024-08-07 11:37:23 +0300

    HTML 5- Day 2

---
+++ HTML5/Day2/demo.js
@@ -0,0 +1,46 @@
+// const canvas = document.getElementById("myCanvas");
+
+// const context = canvas.getContext("2d");
+// console.log(context);
+
+// context.fillStyle = "teal";
+// context.fillRect(10, 10, 100, 75);
+// context.clearRect(25, 25, 50, 50);
+
+//  draw line
+
+// context.strokeStyle = "green";
+// context.lineWidth = 5;
+// context.moveTo(120 , 10); // start position
+// context.lineTo(170, 10);  // end position
+// context.stroke();
+
+// draw vertical line
+// context.strokeStyle = "yellow";
+// context.lineWidth = 3;
+// context.moveTo(125, 20);
+// context.lineTo(125, 60);
+// context.stroke();
+
+// // draw triangle
+// context.strokeStyle = "green";
+// context.lineWidth = 2;
+// context.beginPath();
+// context.moveTo(10, 10);
+// context.lineTo(50, 10);
+// context.lineTo(30, 50);
+// context.closePath();
+// context.stroke();
+// context.fillStyle = "red";
+// context.fill();
+
+// how to write text
+// context.font = "25px sans-serif";
+// context.fillStyle = "blue";
+// context.fillText("Hello ITIians", 30,30);
+
+// how to write outline text
+
+// context.font = "25px Arial";
+// context.strokeStyle = "green";
+// context.strokeText("Hello ITIians", 30, 60);
